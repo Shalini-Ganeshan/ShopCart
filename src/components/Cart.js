3c24a1fd722f09653ab8c34882df4882837cf86e
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-08-05 09:30:18 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -18,17 +18,16 @@ const Cart = ({ cart, setCart, handleChange }) => {
 
   useEffect(() => {
     handlePrice();
-  }, [cart]); 
+  }, [cart]);
 
   return (
-   
-    <article className="bg-white text-black p-4 ">
+    <article className="bg-white text-black p-4">
       {cart?.map((item) => (
         <div className="flex flex-col md:flex-row items-center mb-4 p-4 border-2 border-purple-800 rounded-md" key={item.id}>
-        <div className="flex flex-row items-center flex-shrink-0 font-semibold text-lg">
-          <img src={item.img} alt={item.title} className="w-20 h-20 object-cover rounded-md" />
-          <p className="ml-4 text-center md:text-left">{item.title}</p>
-        </div>
+          <div className="flex flex-row items-center flex-shrink-0 font-semibold text-lg">
+            <img src={item.img} alt={item.title} className="w-20 h-20 object-cover rounded-md" />
+            <p className="ml-4 text-center md:text-left">{item.title}</p>
+          </div>
           <div className="flex flex-col items-center md:items-end md:ml-auto mt-4 md:mt-0 w-full md:w-auto">
             <div className="flex space-x-2 mb-2">
               <button 
@@ -46,23 +45,22 @@ const Cart = ({ cart, setCart, handleChange }) => {
               </button>
             </div>
             <div className="flex flex-col items-end space-y-2">
-      <span className="text-lg font-semibold  mx-auto "> ₹ {item.price}</span>
-      <button 
-        className="bg-red-600 text-white px-4 py-1 rounded hover:bg-red-500" 
-        onClick={() => handleRemove(item.id)}
-      >
-        Remove
-      </button>
-    </div>
+              <span className="text-lg font-semibold mx-auto">₹ {item.price}</span>
+              <button 
+                className="bg-red-600 text-white px-4 py-1 rounded hover:bg-red-500" 
+                onClick={() => handleRemove(item.id)}
+              >
+                Remove
+              </button>
+            </div>
           </div>
         </div>
       ))}
       <div className="mt-4 p-4 border-t border-gray-700 text-center">
-        <span className="text-xl font-semibold ">Total Price of your Cart</span>
-        <span className="block mt-2 text-lg text-blue-500 font-semibold"> ₹  {price}</span>
+        <span className="text-xl font-semibold">Total Price of your Cart</span>
+        <span className="block mt-2 text-lg text-blue-500 font-semibold">₹ {price}</span>
       </div>
     </article>
-
   );
 };
 
