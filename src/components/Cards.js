848bde00a93a1a2c547c1273942e45d6e5512d28
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-08-07 09:58:44 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ const Cards = ({ item, handleClick }) => {
   const { title, author, price, img } = item;
 
   return (
-<div className="border-2 border-purple-300  text-white rounded-lg overflow-hidden shadow-lg m-4 p-4 flex flex-col h-full transition-transform duration-300 transform hover:scale-105">
+<div className="border-2 border-purple-300  text-white rounded-lg overflow-hidden shadow-lg m-4 p-4 flex flex-col h-full ">
   <div className="w-full h-64 bg-gray-700 flex items-center justify-center mb-2">
     <img src={img} alt={title} className="h-full w-full " />
   </div>
